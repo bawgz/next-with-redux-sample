5e29db7471786bf796b9a602cefcfcccd24d29fa
Luke Boggs <lukeboggs7@gmail.com>
2019-02-02 12:24:20 -0500

    add paper to confirmation page -- looks really bad pls fix

--- components/order-confirmation/index.jsx
+++ components/order-confirmation/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 import { confirmedOrderPropTypes } from '../../constants/prop-types';
 
 const OrderConfirmation = ({ order }) => (
   <div>
-    <h1>Order Confirmation</h1>
-    Order Number: {order.id}
+    <Paper elevation={1}>
+      <Typography variant="h3" component="h1">
+        Order Confirmation
+      </Typography>
+      <Typography component="p">Order Number: {order.id}</Typography>
+    </Paper>
   </div>
 );
 
