3df8bba03e0687f474072b3f53e7801c1273a8aa
Luke Boggs <lukeboggs7@gmail.com>
2020-10-24 19:29:00 -0400

    update db name

--- server/data-service.js
+++ server/data-service.js
@@ -5,12 +5,12 @@ const uri = process.env.MONGODB_URI;
 const dataService = {
   getMenu: async () => {
     const db = await mongodb.MongoClient.connect(uri);
-    const collection = db.db('heroku_sp1168cp').collection('menu');
+    const collection = db.db('jarocho').collection('menu');
     return collection.find({}).toArray();
   },
   insertOrder: async order => {
     const db = await mongodb.MongoClient.connect(uri);
-    const collection = db.db('heroku_sp1168cp').collection('orders');
+    const collection = db.db('jarocho').collection('orders');
     await collection.insertOne(order);
     return order;
   },
